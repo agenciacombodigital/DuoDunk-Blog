5e085cf510e06cb1b19646d5a81aa9c2bc042ee5
[dyad] <agenciacombodigital@gmail.com>
2025-10-29 21:57:32 -0300

    [dyad] Removed the calendar page and all related links - wrote 3 file(s), deleted 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ import NBAScoreboardV2 from "./components/NBAScoreboardV2";
 import Times from "./pages/Times";
 import Time from "./pages/Time";
 import Classificacao from './pages/Classificacao';
-import Calendario from './pages/Calendario';
 
 const AppContent = () => {
   const location = useLocation();
@@ -40,7 +39,6 @@ const AppContent = () => {
           <Route path="/times" element={<Times />} />
           <Route path="/times/:teamSlug" element={<Time />} />
           <Route path="/classificacao" element={<Classificacao />} />
-          <Route path="/calendario" element={<Calendario />} />
           
           {/* Admin Routes */}
           <Route path="/admin/login" element={<AdminLogin />} />
--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Menu, X, Home, Newspaper, Users, Trophy, Youtube, Instagram, Twitter, Twitch, Calendar } from 'lucide-react';
+import { Menu, X, Home, Newspaper, Users, Trophy, Youtube, Instagram, Twitter, Twitch } from 'lucide-react';
 
 export default function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,7 +11,6 @@ export default function MobileMenu() {
     { path: '/ultimas', label: 'Últimas', icon: Newspaper },
     { path: '/times', label: 'Times', icon: Users },
     { path: '/classificacao', label: 'Classificação', icon: Trophy },
-    { path: '/calendario', label: 'Calendário', icon: Calendar },
   ];
 
   const toggleMenu = () => setIsOpen(!isOpen);
