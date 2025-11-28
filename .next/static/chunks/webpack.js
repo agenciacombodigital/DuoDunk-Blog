83d8f662bbeb96997ff3a5e337514420d9ea9282
[dyad] <agenciacombodigital@gmail.com>
2025-11-28 08:57:49 -0300

    [dyad] wrote 1 file(s) + extra files edited outside of Dyad

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d0a0583cd66dbe4"; }
+/******/ 		__webpack_require__.h = function() { return "6012846d96158191"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
