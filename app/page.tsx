d404e6f4b6240ba09d0892f4b7b23b7e3a082d62
[dyad] <agenciacombodigital@gmail.com>
2025-12-04 12:52:08 -0300

    [dyad] Ajustando os itens do sidebar para preencherem verticalmente a altura da imagem de destaque. - wrote 1 file(s) + extra files edited outside of Dyad

--- app/page.tsx
+++ app/page.tsx
@@ -113,15 +113,17 @@ export default async function Home() {
           </div>
 
           {/* Sidebar */}
+          {/* Adicionando flex-col e h-full para que o container se estique */}
           <div className="lg:col-span-4 flex flex-col">
             <div className="flex items-center gap-2 border-b-2 border-black pb-2 mb-4">
               <TrendingUp className="text-[#FA007D]" size={20} />
               <h2 className="font-bebas text-2xl text-gray-900">Últimas</h2>
             </div>
-            <div className="flex flex-col justify-between gap-3">
+            {/* O container dos artigos precisa preencher o espaço restante */}
+            <div className="flex flex-col justify-between gap-3 flex-1">
               {sidebarArticles.map((article) => (
-                <Link key={article.id} href={`/artigos/${article.slug}`} className="group flex gap-3 items-stretch h-24">
-                  {/* Ajuste para retangular (16:10) e largura maior (w-32) */}
+                {/* Removendo h-24 e adicionando flex-1 para que cada item se estique */}
+                <Link key={article.id} href={`/artigos/${article.slug}`} className="group flex gap-3 items-stretch flex-1">
                   <div className="relative w-32 shrink-0 rounded-lg overflow-hidden aspect-[16/10] bg-gray-100">
                      <img 
                         src={getOptimizedImageUrl(article.image_url, 400)} 
